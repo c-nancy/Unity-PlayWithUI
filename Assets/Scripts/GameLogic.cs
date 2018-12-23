c0b9ab4d0956297c87ed147db486eaf00e15b8e8
c-nancy <suzlev@outlook.com>
2018-12-23 12:56:07 +0800

    initial commit

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class GameLogic: MonoBehaviour {
+    [SerializeField] int top;
+    [SerializeField] int bottom;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+
+    void Start()
+    {
+        StartGame();
+    }
+
+	void StartGame () {
+        NextGuess();
+    }
+	
+
+    public void OnPressHigher()
+    {
+        if (bottom < 1000) {
+            bottom = guess + 1;
+        }
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        top = guess - 1;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = UnityEngine.Random.Range(bottom, top);
+        guessText.text = guess.ToString();
+        
+    }
+
+
+}
